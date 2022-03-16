409319ab495d35999d04b90f578873f6a82d7e5c
Thomas Aurelius <teg824@hotmail.com>
2022-03-16 02:19:00 -0500

    initial

--- script.js
+++ script.js
@@ -1,27 +1,30 @@
+
 const pokeContainer = document.getElementById('poke-container')
+const pokeStatsContainer = document.getElementById('poke-stats-container')
 const pokemonCount = 150
 const colors = {
    fire: '#d82b34',
    grass: '#1ad148',
 	electric: '#FCF7DE',
-   rock: '#d5d5d4',
-	water: '#DEF3FD',   
-   bug: '#f8d5a3',
-   ghost: '#999',
-   poison: '#837ad6',
+	water: '#DEF3FD',
 	ground: '#f4e7da',
+	rock: '#d5d5d4',
+	fairy: '#fceaff',
+	poison: '#837ad6',
+	bug: '#f8d5a3',
 	dragon: '#97b3e6',
-   ice: '#6eb8e9',
 	psychic: '#eaeda1',
-   fairy: '#fceaff',
-   normal: '#F5F5F5',
 	flying: '#30aae2',
 	fighting: '#E6E0D4',
-	steel: '#979595'
-   
+	normal: '#F5F5F5',
+   steel: '#979595',
+   ice: '#6eb8e9'
 }
 
 const main_types = Object.keys(colors)
+
+
+//original code
 
 const fetchPokemons = async () => {
    for(let i = 1; i <= pokemonCount; i++) {
@@ -57,7 +60,7 @@ const createPokemonCard = (pokemon) => {
 
    const type2 = main_types.find(type => poke_type2.indexOf(type) > -1)
     
-   console.log(type, type2)
+   // console.log(type, type2)
 
    const color = colors[type]
    const color2 = colors[type2]
@@ -80,5 +83,134 @@ const createPokemonCard = (pokemon) => {
    pokemonEl.innerHTML = pokemonInnerHTML
 
    pokeContainer.appendChild(pokemonEl)
+
+
+
+
+
+   
 }
 fetchPokemons()
+
+
+// Stats code
+
+
+
+// Step 1: attach event listeners to each .pokemon div [DONE]
+const pokemonCards = document.querySelectorAll('.pokemon')
+pokemonCards.forEach((card) => {
+   card.addEventListener('click', (e) => {
+      getPokemonName(e)
+      // Step 2: click a .pokemon div to get its ${pokemon.name} [STUCK]
+   })
+})
+function getPokemonName(e) {
+   console.log(e.target.value)
+}
+//Step 3: once you get the name pokemon that you clicked on, update this variable  [READY]
+
+const pokemon_name = "bulbasaur"
+
+
+ 
+// Step 4: use the name to fetch data on that one pokemon [DONE]
+const fetchPokemonStats = async () => {
+     await getPokemonStats(pokemon_name)
+  }
+
+const getPokemonStats = async (name) => {
+   const url = `https://pokeapi.co/api/v2/pokemon/${name}`
+   const res = await fetch(url)
+   const data = await res.json()
+   createPokemonStatCard(data)
+   // console.log(data)
+   
+}
+
+//Step 5: populate the stats card [DONE]
+const createPokemonStatCard = (pokemon) => {
+   const pokemonStatsEl = document.createElement('div')
+   pokemonStatsEl.classList.add('pokemon-detail')
+   
+   const name = pokemon.name[0].toUpperCase() + pokemon.name.slice(1)
+   const ability1 = pokemon.abilities[0].ability.name[0].toUpperCase() + pokemon.abilities[0].ability.name.slice(1)
+   const ability2 = pokemon.abilities[1].ability.name[0].toUpperCase() + pokemon.abilities[1].ability.name.slice(1)
+
+   const id = pokemon.id.toString().padStart(3, '0')
+
+   const poke_type = pokemon.types.map(type => type.type.name)
+   
+   let poke_type2 = ""
+   if (poke_type[1]) {
+      poke_type2 = poke_type[1]
+   } else {
+      poke_type2 = poke_type
+   }
+
+   const type = main_types.find(type => poke_type.indexOf(type) > -1)
+
+   const type2 = main_types.find(type => poke_type2.indexOf(type) > -1)
+    
+   // console.log(type, type2)
+
+   const color = colors[type]
+   const color2 = colors[type2]
+
+   pokemonStatsEl.style.backgroundImage = `linear-gradient(${color2}, ${color})`
+  
+
+   const pokemonStatsInnerHTML = `
+    <div>
+       <div class="img-container">
+         <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${pokemon.id}.svg" alt="">
+          </div>
+          <div class="info">
+         <span class="number">#${id}</span>
+         <h3 class="name">${name}</h3>
+         <small class="type">Type: <span>${poke_type}</span><br></small>
+          </div>
+    </div>
+    <div>
+      <div class="moves">
+      <h3 class="stats">Ability 1: ${ability1}
+      <h3 class="stats">Ability 2: ${ability2}
+      </div>
+       <div class="stats">
+         <h3 class="stats">Hit Points: ${pokemon.stats[0].base_stat}
+         <div class="bar">
+            <div style="width:${pokemon.stats[0].base_stat}%" class="skill">${pokemon.stats[0].base_stat}</div>
+         </div>
+         <h3 class="stats">Attack: ${pokemon.stats[1].base_stat}
+         <div class="bar">
+            <div style="width:${pokemon.stats[1].base_stat}%" class="skill">${pokemon.stats[1].base_stat}</div>
+         </div>
+         <h3 class="stats">Defense: ${pokemon.stats[2].base_stat}
+         <div class="bar">
+            <div style="width:${pokemon.stats[2].base_stat}%" class="skill">${pokemon.stats[2].base_stat}</div>
+         </div>
+         <h3 class="stats">Special-Attack: ${pokemon.stats[3].base_stat}
+         <div class="bar">
+            <div style="width:${pokemon.stats[3].base_stat}%" class="skill">${pokemon.stats[3].base_stat}</div>
+         </div>
+         <h3 class="stats">Special-Defense: ${pokemon.stats[4].base_stat}
+         <div class="bar">
+            <div style="width:${pokemon.stats[4].base_stat}%" class="skill">${pokemon.stats[4].base_stat}</div>
+         </div>
+         <h3 class="stats">Speed: ${pokemon.stats[5].base_stat}
+         <div class="bar">
+            <div style="width:${pokemon.stats[5].base_stat}%" class="skill">${pokemon.stats[5].base_stat}</div>
+         </div>
+       
+       </div>
+    </div>
+   `
+
+   pokemonStatsEl.innerHTML = pokemonStatsInnerHTML
+
+   pokeStatsContainer.appendChild(pokemonStatsEl)
+
+}
+
+
+fetchPokemonStats()
